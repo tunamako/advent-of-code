75f3772a6db3b9ab08262b9e2c4315ce0dbb544b
tunamako <inedibledelicacies@gmail.com>
2021-12-03 00:55:43 -0600

    day3

--- aoc-2021/python/day03/day03.py
+++ aoc-2021/python/day03/day03.py
@@ -8,21 +8,48 @@ import math
 
 
 YEAR = 2021
-DAY = 1
+DAY = 3
+
+
+def get_commonality(data, index):
+    ones = sum([int(string[index]) for string in data])
+    zeroes = len(data) - ones
+
+    return str(int(ones >= zeroes)), str(int(ones < zeroes))
 
 
 def part_one(_input):
-    pass
+    gamma, epsilon = "", ""
+
+    for i in range(len(_input[0])):
+        most, least = get_commonality(_input, i)
+        gamma += most
+        epsilon += least
+
+    return int(gamma, 2) * int(epsilon, 2)
+
+
+def reduce(data, use_most):
+    for i in range(len(next(iter(data), None))):
+        if len(data) == 1:
+            break
+
+        most, least = get_commonality(data, i)
+        criteria = most if use_most else least
+
+        data = {string for string in data if string[i] == criteria}
+
+    return int(data.pop(), 2)
 
 
 def part_two(_input):
-    pass
+    return reduce(set(_input), True) * reduce(set(_input), False)
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
     _input = puzzle.input_data.split('\n')
-    #_input = open('bigboy').readlines()
+    #_input = [line[:-1] for line in open('input').readlines()]
 
     print(part_one(_input))
     print(part_two(_input))
