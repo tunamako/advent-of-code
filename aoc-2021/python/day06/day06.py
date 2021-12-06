5576c1cb7e092a1b87a6c7ee0aa25a5d1773a564
tunamako <inedibledelicacies@gmail.com>
2021-12-05 23:48:29 -0600

    day 6

--- aoc-2021/python/day06/day06.py
+++ aoc-2021/python/day06/day06.py
@@ -5,52 +5,41 @@ from collections import defaultdict, namedtuple, Counter, deque
 from itertools import permutations, combinations, chain
 import re
 import math
-
+from pprint import *
 
 YEAR = 2021
 DAY = 6
 
+def simulate(_input, days):
+    fishes = Counter(map(int, _input.split(',')))
+
+    for i in range(days):
+        new_fish = fishes[0]
+        fishes[0] = 0
+
+        for age in range(1, 9):
+            fishes[age - 1] += fishes[age]
+            fishes[age] = 0
+
+        fishes[8] = new_fish
+        fishes[6] += new_fish
+
+    return sum(fishes.values())
+
 
 def part_one(_input):
-    fishes = list(map(int, _input.split(',')))
-    print(_input)
-
-    for i in range(80):
-        print(i)
-        to_add = []
-        for j in range(len(fishes)):
-            if fishes[j] == 0:
-                to_add.append(8)
-                fishes[j] = 6
-            else:
-                fishes[j] -= 1
-        fishes += to_add
-    return len(fishes)
+    return simulate(_input, 18)
 
 def part_two(_input):
-    fishes = list(map(int, _input.split(',')))
-
-    for i in range(80):
-        #print(fishes)
-        to_add = []
-        for j in range(len(fishes)):
-            if fishes[j] == 0:
-                to_add.append(8)
-                fishes[j] = 6
-            else:
-                fishes[j] -= 1
-
-        fishes += to_add
-    return len(fishes)
-
+    return simulate(_input, 256)
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
     _input = puzzle.input_data.split('\n')[0]
-    #_input = open('input').readlines()[0]
+    _input = open('input').readlines()[0]
 
-    #print(part_one(_input))
-    #print(part_two(_input))
+    print(part_one(_input))
+    print(part_two(_input))
 
     #cProfile.run('print(part_one(_input))')
-    cProfile.run('print(part_two(_input))')
+    #cProfile.run('print(part_two(_input))')
