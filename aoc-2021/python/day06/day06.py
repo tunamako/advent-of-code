989ed407d2b29e124c9845bb5e4f25303a3aad53
tunamako <inedibledelicacies@gmail.com>
2021-12-05 23:09:55 -0600

    day 6 in progress

--- aoc-2021/python/day06/day06.py
+++ aoc-2021/python/day06/day06.py
@@ -8,24 +8,49 @@ import math
 
 
 YEAR = 2021
-DAY = 1
+DAY = 6
 
 
 def part_one(_input):
-    pass
+    fishes = list(map(int, _input.split(',')))
+    print(_input)
 
+    for i in range(80):
+        print(i)
+        to_add = []
+        for j in range(len(fishes)):
+            if fishes[j] == 0:
+                to_add.append(8)
+                fishes[j] = 6
+            else:
+                fishes[j] -= 1
+        fishes += to_add
+    return len(fishes)
 
 def part_two(_input):
-    pass
+    fishes = list(map(int, _input.split(',')))
+
+    for i in range(80):
+        #print(fishes)
+        to_add = []
+        for j in range(len(fishes)):
+            if fishes[j] == 0:
+                to_add.append(8)
+                fishes[j] = 6
+            else:
+                fishes[j] -= 1
+
+        fishes += to_add
+    return len(fishes)
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
-    _input = puzzle.input_data.split('\n')
-    #_input = open('bigboy').readlines()
+    _input = puzzle.input_data.split('\n')[0]
+    #_input = open('input').readlines()[0]
 
-    print(part_one(_input))
-    print(part_two(_input))
+    #print(part_one(_input))
+    #print(part_two(_input))
 
     #cProfile.run('print(part_one(_input))')
-    #cProfile.run('print(part_two(_input))')
+    cProfile.run('print(part_two(_input))')
