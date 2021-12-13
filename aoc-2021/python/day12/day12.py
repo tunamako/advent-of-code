737d3fe045ece8da1ec47133ee7106afb081343b
tunamako <inedibledelicacies@gmail.com>
2021-12-13 01:52:03 -0600

    day 12/13

--- aoc-2021/python/day12/day12.py
+++ aoc-2021/python/day12/day12.py
@@ -5,27 +5,75 @@ from collections import defaultdict, namedtuple, Counter, deque
 from itertools import permutations, combinations, chain
 import re
 import math
-
+from pprint import *
 
 YEAR = 2021
-DAY = 1
+DAY = 12
+
+
+def parse_caves(cave_list):
+    caves = defaultdict(set)
+
+    for line in cave_list:
+        lhs, rhs = line.split('-')
+        if rhs != "start": caves[lhs].add(rhs)
+        if lhs != "start": caves[rhs].add(lhs)
+
+    return caves
+
+
+def gen_paths(caves, cur="start", cur_path="start"):
+    if cur == "end":
+        return 1
+
+    cur_path += cur
+    paths = 0
+
+    for adj in caves[cur]:
+        if (adj.islower() and adj in cur_path):
+            continue
+        paths += gen_paths(caves, adj, cur_path)
+
+    return paths
 
 
 def part_one(_input):
-    pass
+    caves = parse_caves(_input)
+    return gen_paths(caves, "start")
+
+
+def gen_paths2(caves, cur, cur_path=defaultdict(int), used_second=False):
+    if cur == "end":
+        return 1
+
+    cur_path[cur] += 1
+    paths = 0
+
+    for adj in caves[cur]:
+        if adj.islower() and cur_path[adj]:
+            if used_second:
+                continue
+            else:
+                paths += gen_paths2(caves, adj, cur_path, True)
+        else:
+            paths += gen_paths2(caves, adj, cur_path, used_second)
+
+    cur_path[cur] -= 1
+    return paths
 
 
 def part_two(_input):
-    pass
+    caves = parse_caves(_input)
+    return gen_paths2(caves, "start")
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
     _input = puzzle.input_data.split('\n')
-    #_input = open('bigboy').readlines()
+    #_input = [line[:-1] for 1line in open('input').readlines()]
 
     print(part_one(_input))
     print(part_two(_input))
 
-    #cProfile.run('print(part_one(_input))')
-    #cProfile.run('print(part_two(_input))')
+    #cProfile.run('part_one(_input)')
+    #cProfile.run('part_two(_input)')
--- aoc-2021/python/day13/day13.py
+++ aoc-2021/python/day13/day13.py
@@ -5,27 +5,80 @@ from collections import defaultdict, namedtuple, Counter, deque
 from itertools import permutations, combinations, chain
 import re
 import math
-
+import numpy as np
 
 YEAR = 2021
-DAY = 1
+DAY = 13
+
+
+def parse_input(_input):
+    split = _input.index('')
+    dots = []
+    for line in _input[:split]:
+        x, y = line.split(',')
+        dots.append({'x': int(x), 'y': int(y)})
+
+    folds = []
+    for line in _input[split+1:]:
+        f = line.split(' ')[-1].split('=')
+        f = (f[0], int(f[1]))
+        folds.append(f)
+
+    return dots, folds
+
+
+def prune_dupes(dots):
+    unique = set()
+    for d in dots:
+        unique.add((d['x'], d['y']))
+
+    return [{'x': int(d[0]), 'y': int(d[1])} for d in unique]
 
 
 def part_one(_input):
-    pass
+    dots, folds = parse_input(_input)
+    f = folds[0]
+    for dot in filter(lambda d: d[f[0]] > f[1], dots):
+        delta = dot[f[0]] - f[1]
+        dot[f[0]] -= 2 * delta
+
+    dots = prune_dupes(dots)
+    return len(dots)
+
+
+def print_paper(dots):
+    max_x = max([d['x'] for d in dots])
+    max_y = max([d['y'] for d in dots])
+
+    paper = np.full((max_x + 1, max_y + 1), ' ', )
+    for d in dots:
+        paper[d['x']][d['y']] = '#'
+
+    paper = np.swapaxes(paper, 0, 1)
+    for line in paper:
+        print(''.join(line))
 
 
 def part_two(_input):
-    pass
+    dots, folds = parse_input(_input)
+
+    for f in folds:
+        for dot in filter(lambda d: d[f[0]] > f[1], dots):
+            delta = dot[f[0]] - f[1]
+            dot[f[0]] -= 2 * delta
+
+        dots = prune_dupes(dots)
+
+    print_paper(dots)
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
     _input = puzzle.input_data.split('\n')
-    #_input = open('bigboy').readlines()
+    #_input = [line[:-1] for line in open('input').readlines()]
 
     print(part_one(_input))
-    print(part_two(_input))
+    part_two(_input)
 
     #cProfile.run('print(part_one(_input))')
     #cProfile.run('print(part_two(_input))')
