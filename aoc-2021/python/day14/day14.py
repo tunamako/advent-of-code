97d9e7fce25fc55cb769b0be85de23d5ff3acf74
tunamako <inedibledelicacies@gmail.com>
2021-12-14 18:16:33 -0600

    day14

--- aoc-2021/python/day14/day14.py
+++ aoc-2021/python/day14/day14.py
@@ -5,24 +5,53 @@ from collections import defaultdict, namedtuple, Counter, deque
 from itertools import permutations, combinations, chain
 import re
 import math
-
+import numpy as np
+from pprint import *
+import sys
+sys.setrecursionlimit(30000)
 
 YEAR = 2021
-DAY = 1
+DAY = 14
+
+
+def solve(_input, step_count):
+    template  = _input[0]
+    pair_count = Counter([''.join(pair) for pair in zip(template, template[1:])])
+    rules = [rule.split(' -> ') for rule in _input[2:]]
+
+    for step in range(step_count):
+        changes = Counter()
+        for rule in rules:
+            if pair_count[rule[0]] <= 0:
+                continue
+
+            count = pair_count[rule[0]]
+            changes[rule[0]] += -1 * count
+            changes[rule[0][0] + rule[1]] += count
+            changes[rule[1] + rule[0][1]] += count
+
+        for pair, change in changes.items():
+            pair_count[pair] += change
+
+    total_count = Counter()
+    for pair, count in pair_count.items():
+        total_count[pair[0]] += count
+
+    return total_count.most_common()[0][1] - total_count.most_common()[-1][1] + 1
 
 
 def part_one(_input):
-    pass
+    return solve(_input, 10)
 
 
 def part_two(_input):
-    pass
+    return solve(_input, 40)
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
     _input = puzzle.input_data.split('\n')
-    #_input = open('bigboy').readlines()
+    #_input = [line[:-1] for line in open('input').readlines()]
 
     print(part_one(_input))
     print(part_two(_input))
