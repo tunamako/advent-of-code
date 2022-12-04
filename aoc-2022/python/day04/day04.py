9ecf49d2a2653c2eb51c190acc1c2f7d81eb1c91
tunamako <inedibledelicacies@gmail.com>
2022-12-04 00:15:21 -0600

    day 4

--- aoc-2022/python/day04/day04.py
+++ aoc-2022/python/day04/day04.py
@@ -10,22 +10,28 @@ import numpy as np
 import sys
 from copy import deepcopy
 
-YEAR = 2021
-DAY = 1
+YEAR = 2022
+DAY = 4
 
+def parse_pairs(_input):
+    _input = [list(map(int, re.split(',|-', line))) for line in _input]
+
+    return [(set(range(pair[0], pair[1] + 1)), set(range(pair[2], pair[3] + 1))) for pair in _input]
 
 def part_one(_input):
-    print(_input)
-
+    pairs = parse_pairs(_input)
+    return sum((pair[0] <= pair[1] or pair[1] <= pair[0]) for pair in pairs)
 
 def part_two(_input):
-    pass
+    pairs = parse_pairs(_input)
+    return sum(bool(pair[0] & pair[1]) for pair in pairs)
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
     _input = puzzle.input_data.split('\n')
     #_input = [line[:-1] for line in open('input').readlines()]
+
 
     print(part_one(_input))
     print(part_two(_input))
