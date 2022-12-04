ae5c3dc92156a98eb0be64a52bd06d28cd87f39c
tunamako <inedibledelicacies@gmail.com>
2022-12-03 23:57:21 -0600

    day 3

--- aoc-2022/python/day03/day03.py
+++ aoc-2022/python/day03/day03.py
@@ -10,17 +10,35 @@ import numpy as np
 import sys
 from copy import deepcopy
 
-YEAR = 2021
-DAY = 1
+YEAR = 2022
+DAY = 3
 
+
+def priority(item):
+    return ord(item) - (38 if item.isupper() else 96)
+
+def parse_sacks(_input):
+    sacks = []
+    for sack in _input:
+        midpoint = int(len(sack)/2)
+        comp_1 = set(map(priority, sack[:midpoint]))
+        comp_2 = set(map(priority, sack[midpoint:]))
+        sacks.append((comp_1, comp_2, comp_1.union(comp_2)))
+
+    return sacks
 
 def part_one(_input):
-    print(_input)
-
+    sacks = parse_sacks(_input)
+    return sum(sack[0].intersection(sack[1]).pop() for sack in sacks)
 
 def part_two(_input):
-    pass
+    sacks = parse_sacks(_input)
 
+    total = 0
+    for i in range(0, len(sacks), 3):
+        total += sacks[i][2].intersection(sacks[i+1][2], sacks[i+2][2]).pop()
+
+    return total
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
