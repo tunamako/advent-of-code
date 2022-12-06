f84adee40e218237b50dc4aff82b52897420fb56
tunamako <inedibledelicacies@gmail.com>
2022-12-05 23:09:29 -0600

    day 6

--- aoc-2022/python/day06/day06.py
+++ aoc-2022/python/day06/day06.py
@@ -10,21 +10,24 @@ import numpy as np
 import sys
 from copy import deepcopy
 
-YEAR = 2021
-DAY = 1
+YEAR = 2022
+DAY = 6
 
 
 def part_one(_input):
-    print(_input)
-
+    for i in range(len(_input)):
+        if len(set(_input[i:i+4])) == 4:
+            return i + 4
 
 def part_two(_input):
-    pass
+    for i in range(len(_input)):
+        if len(set(_input[i:i+14])) == 14:
+            return i + 14
 
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
-    _input = puzzle.input_data.split('\n')
+    _input = puzzle.input_data.split('\n')[0]
     #_input = [line[:-1] for line in open('input').readlines()]
 
     print(part_one(_input))
