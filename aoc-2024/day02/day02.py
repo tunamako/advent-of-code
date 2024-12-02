09f758677f649d4ce648c7f550b066fe1bbef502
tunamako <inedibledelicacies@gmail.com>
2024-12-02 14:54:44 -0600

    2024 day 2

---
+++ aoc-2024/day02/day02.py
@@ -0,0 +1,65 @@
+from aocd.models import Puzzle
+
+import cProfile
+from collections import defaultdict, namedtuple, Counter, deque
+from itertools import permutations, combinations, chain, tee
+import re
+import math
+from pprint import pprint
+import numpy as np
+import sys
+from copy import deepcopy
+
+YEAR = 2024
+DAY = 2
+
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return list(zip(a, b))
+
+def is_safe(report):
+    adj_pairs = pairwise(report)
+    # check ascending or descending
+    if all(pair[0] > pair[1] for pair in adj_pairs) or all(pair[0] < pair[1] for pair in adj_pairs):
+        # check gap sizes
+        if all(0 < abs(pair[0] - pair[1]) < 4 for pair in adj_pairs):
+            return True
+    
+    return False
+
+def part_one(_input):
+    reports = [list(map(int, report.split(' '))) for report in _input]
+    return sum(is_safe(report) for report in reports)
+
+def part_two(_input):
+    reports = [list(map(int, report.split(' '))) for report in _input]
+    ret = 0
+
+    for report in reports:
+        if is_safe(report):
+            ret += 1
+        else:
+            for i in range(len(report)):
+                tmp = report[::]
+                del tmp[i]
+
+                if is_safe(tmp):
+                    ret += 1
+                    break
+    
+    return ret
+
+
+
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=YEAR, day=DAY)
+    _input = puzzle.input_data.split('\n')
+    #_input = [line[:-1] for line in open('input').readlines()]
+
+    print(part_one(_input))
+    print(part_two(_input))
+
+    #cProfile.run('print(part_one(_input))')
+    #cProfile.run('print(part_two(_input))')
