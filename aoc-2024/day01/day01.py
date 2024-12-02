5d6e57f157bdfacf865eb2f44bc8f746ee945dde
tunamako <inedibledelicacies@gmail.com>
2024-12-02 14:17:48 -0600

    2024 day 1

--- aoc-2024/day01/day01.py
+++ aoc-2024/day01/day01.py
@@ -15,12 +15,18 @@ DAY = 1
 
 
 def part_one(_input):
-    print(_input)
+    list_1 = sorted([int(entry.split('   ')[0]) for entry in _input])
+    list_2 = sorted([int(entry.split('   ')[1]) for entry in _input])
 
-
+    return sum([abs(list_1[i] - list_2[i]) for i in range(len(list_1))])
+    
 def part_two(_input):
-    pass
+    list_1 = sorted([int(entry.split('   ')[0]) for entry in _input])
+    list_2 = sorted([int(entry.split('   ')[1]) for entry in _input])
 
+    count = Counter(list_2)
+
+    return sum([item * count[item] for item in list_1])
 
 if __name__ == '__main__':
     puzzle = Puzzle(year=YEAR, day=DAY)
